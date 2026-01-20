e03e4cc8a1dd2ed36ba2df1c5519e1ea96098dc6
davibern <david.bern.pal@gmail.com>
2026-01-20 20:41:23 +0100

    Añadir comentarios

--- src/exportar_datos.py
+++ src/exportar_datos.py
@@ -7,5 +7,8 @@ class ExportarDatos:
     def __init__(self, datos: CargarFichero):
         self.datos = datos
     
-    def exportar(self):
+    def exportar(self) -> None:
+        """
+        Exporta los datos a un fichero parquet
+        """
         self.datos.df.to_parquet("data/finanzas.parquet", engine="pyarrow", compression="snappy")
