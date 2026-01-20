84d0ccde510f126699093c993c183b21b8ba15cb
davibern <david.bern.pal@gmail.com>
2026-01-20 20:40:15 +0100

    Actualizar main.py para comprobar modificaciones

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from src.cargar_fichero import CargarFichero
+from src.exportar_datos import ExportarDatos
+import pandas as pd
 
 fichero = CargarFichero("raw/202512.xml")
 fichero.parsear_xml()
 fichero.limpiar_datos()
-print(fichero.df)
+
+exportar = ExportarDatos(fichero)
+exportar.exportar()
+
+df = pd.read_parquet("data/finanzas.parquet")
+print(df)
