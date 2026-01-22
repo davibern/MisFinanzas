6a4d4bf00a63fc59daa59e7a201fe8c9e903b288
davibern <david.bern.pal@gmail.com>
2026-01-22 19:41:42 +0100

    Adaptar main.py para probar nuevas funcionalidades de mis_finanzas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.cargar_fichero import CargarFichero
 from src.exportar_datos import ExportarDatos
+from src.mis_finanzas import MisFinanzas
 
 fichero = CargarFichero("raw/202512.xml")
 fichero.parsear_xml()
@@ -16,3 +17,10 @@ print(df)
 print("\nFichero parquet año 2025 y mes 12\n")
 df_año = pd.read_parquet("data/finanzas.parquet", filters=[("año", "==", 2025), ("mes", "==", 12)])
 print(df_año)
+
+mis_finanzas = MisFinanzas(df, 11, 2025)
+ingresos = mis_finanzas.obtener_ingresos_mes_año()
+print(f"\nIngresos del mes: {ingresos}")
+gastos = mis_finanzas.obtener_gastos_mes_año()
+print(f"Gastos del mes: {gastos}")
+print(f"Balance del mes: {ingresos + gastos}")
