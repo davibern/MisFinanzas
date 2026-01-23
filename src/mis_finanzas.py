5fec5eec569ff118823afd78f756553046b31871
davibern <david.bern.pal@gmail.com>
2026-01-23 18:48:09 +0100

    Crear métodos de estudios por intervalos y corregir importación numérica

--- src/mis_finanzas.py
+++ src/mis_finanzas.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 class MisFinanzas():
 
-    def __init__(self, mes: int, año: int) -> None:
+    def __init__(self) -> None:
         self.datos = pd.read_parquet("data/finanzas.parquet")
-        self.mes = mes
-        self.año = año
+        # Convertir columnas de partición de categorical a int
+        self.datos['año'] = self.datos['año'].astype(int)
+        self.datos['mes'] = self.datos['mes'].astype(int)
 
-    def obtener_ingresos_mes_año(self) -> float:
+    def obtener_ingresos_mes_año(self, año: int, mes: int) -> float:
         """
         Obtener los ingresos totales del mes de estudio
 
@@ -16,12 +17,12 @@ class MisFinanzas():
             float: total de ingresos del mes
         """
         return self.datos[
-            (self.datos['año'] == self.año) &
-            (self.datos['mes'] == self.mes) &
+            (self.datos['año'] == año) &
+            (self.datos['mes'] == mes) &
             (self.datos['tipo'] == 'Ingreso')
         ]['importe'].sum()
 
-    def obtener_gastos_mes_año(self) -> float:
+    def obtener_gastos_mes_año(self, año: int, mes: int) -> float:
         """
         Obtener los gastos totales del mes de estudio
 
@@ -29,7 +30,45 @@ class MisFinanzas():
             float: total de gastos del mes
         """
         return self.datos[
-            (self.datos['año'] == self.año) &
-            (self.datos['mes'] == self.mes) &
+            (self.datos['año'] == año) &
+            (self.datos['mes'] == mes) &
             (self.datos['tipo'] == 'Gasto')
+        ]['importe'].sum()
+
+    def obtener_intervalo_gastos(self, año: int, mes_inicio: int, mes_fin: int) -> float:
+        """
+        Obtener los gastos totales en un intervalo de fecha
+
+        Args:
+            año (int): el año de estudio
+            mes_inicio (int): el mes inicial
+            mes_fin (int): el mes final
+
+        Returns:
+            float: total de gastos imputados en el periodo de estudio
+        """
+        return self.datos[
+            (self.datos['año'] == año) &
+            (self.datos['mes'] >= mes_inicio) &
+            (self.datos['mes'] <= mes_fin) &
+            (self.datos['tipo'] == 'Gasto')
+        ]['importe'].sum()
+
+    def obtener_intervalo_ingresos(self, año: int, mes_inicio: int, mes_fin: int) -> float:
+        """
+        Obtener los ingresos totales en un intervalo de fecha
+
+        Args:
+            año (int): el año de estudio
+            mes_inicio (int): el mes inicial
+            mes_fin (int): el mes final
+
+        Returns:
+            float: total de ingresos imputados en el periodo de estudio
+        """
+        return self.datos[
+            (self.datos['año'] == año) &
+            (self.datos['mes'] >= mes_inicio) &
+            (self.datos['mes'] <= mes_fin) &
+            (self.datos['tipo'] == 'Ingreso')
         ]['importe'].sum()
