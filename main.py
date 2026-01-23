8a967900853a5c3ab28527ac712951464104f4c5
davibern <david.bern.pal@gmail.com>
2026-01-23 18:51:29 +0100

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-import pandas as pd
 from src.cargar_fichero import CargarFichero
 from src.exportar_datos import ExportarDatos
 from src.mis_finanzas import MisFinanzas
 
-fichero = CargarFichero("raw/202512.xml")
-fichero.parsear_xml()
-fichero.limpiar_datos()
+cargar: bool = False
+if cargar:
+    for mes in range(1, 13):
+        fichero = CargarFichero(f"raw/2025{mes:02d}.xml")
+        fichero.parsear_xml()
+        fichero.limpiar_datos()
+        exportar = ExportarDatos(fichero)
+        exportar.exportar()
 
-exportar = ExportarDatos(fichero)
-exportar.exportar()
+mes_11 = MisFinanzas()
+ingresos_11 = mes_11.obtener_ingresos_mes_año(2025, 11)
+gastos_11 = mes_11.obtener_gastos_mes_año(2025, 11)
+mes_12 = MisFinanzas()
+ingresos_12 = mes_12.obtener_ingresos_mes_año(2025, 12)
+gastos_12 = mes_12.obtener_gastos_mes_año(2025, 12)
+print(f"\nIngresos del mes 11: {ingresos_11}")
+print(f"\nGastos del mes 11: {gastos_11}")
+print(f"\nIngresos del mes 12: {ingresos_12}")
+print(f"\nGastos del mes 12: {gastos_12}")
 
-print("Todo el fichero parquet\n")
-df = pd.read_parquet("data/finanzas.parquet")
-print(df)
-
-print("\nFichero parquet año 2025 y mes 12\n")
-df_año = pd.read_parquet("data/finanzas.parquet", filters=[("año", "==", 2025), ("mes", "==", 12)])
-print(df_año)
-
-mis_finanzas = MisFinanzas(11, 2025)
-ingresos = mis_finanzas.obtener_ingresos_mes_año()
-print(f"\nIngresos del mes: {ingresos}")
-gastos = mis_finanzas.obtener_gastos_mes_año()
-print(f"Gastos del mes: {gastos}")
-print(f"Balance del mes: {ingresos + gastos}")
+finanzas = MisFinanzas()
+intervalo_gastos = finanzas.obtener_intervalo_gastos(2025, 11, 12)
+intervalo_ingresos = finanzas.obtener_intervalo_ingresos(2025, 11, 12)
+print(intervalo_gastos)
+print(intervalo_ingresos)
