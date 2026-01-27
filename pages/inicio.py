1a4324100c2dbdaa7a0764063bf24d6410e70f81
davibern <david.bern.pal@gmail.com>
2026-01-27 20:05:05 +0100

    Añadir imagen central

--- pages/inicio.py
+++ pages/inicio.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 st.title("📊 Mis Finanzas")
+image = Image.open("./assets/logo.png")
+st.image(image, width=900)
 st.write("Bienvenido a la página de inicio de Mis Finanzas.")
 st.write("Aquí puedes gestionar y analizar tus finanzas personales de manera eficiente.")
