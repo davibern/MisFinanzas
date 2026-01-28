3873c8c2e7f9f5394cb7618cd4b4c18ff3e1f865
davibern <david.bern.pal@gmail.com>
2026-01-28 19:07:22 +0100

    Bajar el tamaño de logo.png

--- pages/inicio.py
+++ pages/inicio.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title("📊 Mis Finanzas")
-image = Image.open("./assets/logo.png")
+image = Image.open("./assets/logo.webp")
 st.image(image, width=900)
 st.write("Bienvenido a la página de inicio de Mis Finanzas.")
 st.write("Aquí puedes gestionar y analizar tus finanzas personales de manera eficiente.")
