e38158e0d947074aa18b5766d37fc0970efd136e
davibern <david.bern.pal@gmail.com>
2026-01-28 19:01:32 +0100

    Refactorizar código

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 
-inicio_pagina = st.Page("./pages/inicio.py", title="Inicio", icon=":material/home:")
-cargar_pagina = st.Page("./pages/cargar_datos.py", title="Cargar Datos", icon=":material/file_upload:")
-datos_mes_pagina = st.Page("./pages/datos_por_mes.py", title="Datos del Mes", icon=":material/calendar_month:")
+def main():
+    inicio_pagina = st.Page("./pages/inicio.py", title="Inicio", icon=":material/home:")
+    cargar_pagina = st.Page("./pages/cargar_datos.py", title="Cargar Datos", icon=":material/file_upload:")
+    datos_mes_pagina = st.Page("./pages/datos_por_mes.py", title="Datos del Mes", icon=":material/calendar_month:")
 
-pg = st.navigation(
-        {
-            "Inicio": [inicio_pagina],
-            "Informes": [datos_mes_pagina],
-            "Herramientas": [cargar_pagina],
-        }
+    pg = st.navigation(
+            {
+                "Inicio": [inicio_pagina],
+                "Informes": [datos_mes_pagina],
+                "Herramientas": [cargar_pagina],
+            }
+        )
+
+    st.set_page_config(
+        page_title="Mis Finanzas",
+        page_icon=":material/account_balance_wallet:",
+        layout="wide"
     )
-st.set_page_config(page_title="Mis Funciones", page_icon=":material/leaderboard:", layout="wide")
 
-pg.run()
+    pg.run()
+
+
+if __name__ == "__main__":
+    main()
