31f536438ecba25a6babbaeec921063d212a497b
davibern <david.bern.pal@gmail.com>
2026-01-31 15:47:29 +0100

    Añadir acceso a página de datos mensuales

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ def main():
     inicio_pagina = st.Page("./pages/inicio.py", title="Inicio", icon=":material/home:")
     cargar_pagina = st.Page("./pages/cargar_datos.py", title="Cargar Datos", icon=":material/file_upload:")
     datos_mes_pagina = st.Page("./pages/datos_por_mes.py", title="Resumen Mensual", icon=":material/stacked_bar_chart:")
+    datos_por_meses = st.Page("./pages/datos_por_meses.py", title="Datos por Meses", icon=":material/bar_chart:")
 
     pg = st.navigation(
             {
                 "Inicio": [inicio_pagina],
-                "Informes": [datos_mes_pagina],
+                "Informes": [datos_mes_pagina, datos_por_meses],
                 "Herramientas": [cargar_pagina],
             }
         )
