c063cd457f19d0a7a891ab12a0bc234c66c0ff96
davibern <david.bern.pal@gmail.com>
2026-02-04 17:44:46 +0100

    Cambiar nombres de menú y archivos

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import streamlit as st
 def main():
     inicio_pagina = st.Page("./pages/inicio.py", title="Inicio", icon=":material/home:")
     cargar_pagina = st.Page("./pages/cargar_datos.py", title="Cargar Datos", icon=":material/file_upload:")
-    datos_mes_pagina = st.Page("./pages/datos_por_mes.py", title="Resumen Mensual", icon=":material/stacked_bar_chart:")
-    datos_por_meses = st.Page("./pages/datos_por_meses.py", title="Datos por Meses", icon=":material/bar_chart:")
+    resumen_mensual = st.Page("./pages/resumen_mensual.py", title="Resumen Mensual", icon=":material/stacked_bar_chart:")
+    resumen_anual = st.Page("./pages/resumen_anual.py", title="Resumen Anual", icon=":material/bar_chart:")
 
     pg = st.navigation(
             {
                 "Inicio": [inicio_pagina],
-                "Informes": [datos_mes_pagina, datos_por_meses],
+                "Informes": [resumen_mensual, resumen_anual],
                 "Herramientas": [cargar_pagina],
             }
         )
