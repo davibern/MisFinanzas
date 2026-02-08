01ac70a66420089cca63511db2ee3b23e6b31d43
davibern <david.bern.pal@gmail.com>
2026-02-08 12:09:28 +0100

    Validar que exista nombre y que esté en el formato correcto para guardar la copia del fichero

--- pages/cargar_datos.py
+++ pages/cargar_datos.py
@@ -12,29 +12,35 @@ st.markdown("""
 2. A continuación, se debe seleccionar el archivo `.xml` que se va a cargar.
 """)
 
-nombre_archivo = st.text_input("Nombre del archivo (formato: YYYYMM)")
+nombre_archivo = str(st.text_input("Nombre del archivo (formato: YYYYMM)", placeholder="YYYYMM"))
 
+# Validar que exista un nombre y luego que siga el formato correcto
 if nombre_archivo:
-    archivo = st.file_uploader("Selecciona un archivo XML", type=["xml"])
+    if nombre_archivo.isalpha():
+        st.warning("El nombre del fichero debe ser numérico y seguir el formato YYYYMM, ejemplo: 202401, que sería enero 2024.")
+    elif len(nombre_archivo) != 6:
+        st.warning("El nombre del fichero debe tener 6 dígitos y seguir el formato YYYYMM, ejemplo: 202401, que sería enero 2024.")
+    else:
+        archivo = st.file_uploader("Selecciona un archivo XML", type=["xml"])
 
-    if archivo is not None:
-        # Cargar y procesar el archivo desde el uploader de Streamlit
-        fichero = CargarFichero(archivo)
-        fichero.parsear_xml()
-        fichero.limpiar_datos()
-        exportar = ExportarDatos(fichero)
-        resultado = exportar.exportar()
+        if archivo is not None:
+            # Cargar y procesar el archivo desde el uploader de Streamlit
+            fichero = CargarFichero(archivo)
+            fichero.parsear_xml()
+            fichero.limpiar_datos()
+            exportar = ExportarDatos(fichero)
+            resultado = exportar.exportar()
 
-        # Comprobar resultado
-        if resultado == 0:
-            st.warning("El año y el mes ya han sido procesados anteriormente.")
-        else:
-            # Crear directorio raw si no existe
-            if not os.path.exists("./raw"):
-                os.makedirs("./raw")
+            # Comprobar resultado
+            if resultado == 0:
+                st.warning("El año y el mes ya han sido procesados anteriormente.")
+            else:
+                # Crear directorio raw si no existe
+                if not os.path.exists("./raw"):
+                    os.makedirs("./raw")
 
-            # Guardar el archivo original en la carpeta raw como copia de seguridad con el nombre indicado
-            nombre_archivo = f"{nombre_archivo}.xml" if nombre_archivo else archivo.name
-            with open(os.path.join("./raw", nombre_archivo), "wb") as f:
-                f.write(archivo.getbuffer())
-            st.success("Datos exportados correctamente. Se ha guardado una copia en la carpeta raw y los datos se han añadido al parquet.")
+                # Guardar el archivo original en la carpeta raw como copia de seguridad con el nombre indicado
+                nombre_archivo = f"{nombre_archivo}.xml" if nombre_archivo else archivo.name
+                with open(os.path.join("./raw", nombre_archivo), "wb") as f:
+                    f.write(archivo.getbuffer())
+                st.success("Datos exportados correctamente. Se ha guardado una copia en la carpeta raw y los datos se han añadido al parquet.")
