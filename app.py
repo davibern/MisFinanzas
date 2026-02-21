99ff4b2cead59743283a222296e98b6d01637271
davibern <david.bern.pal@gmail.com>
2026-02-21 12:40:12 +0100

    Incluir en el menú de navegación acceso directo al manual de ayuda

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ def main():
     resumen_mensual = st.Page("./pages/resumen_mensual.py", title="Resumen Mensual", icon=":material/stacked_bar_chart:")
     resumen_anual = st.Page("./pages/resumen_anual.py", title="Resumen Anual", icon=":material/bar_chart:")
     ahorro = st.Page("./pages/ahorro.py", title="Ahorro", icon=":material/savings:")
+    cargar_pagina_ayuda = st.Page("./pages/cargar_datos_ayuda.py", title="Cargar Datos", icon=":material/help:")
 
     pg = st.navigation(
             {
                 "Inicio": [inicio_pagina],
                 "Informes": [resumen_mensual, resumen_anual, ahorro],
                 "Herramientas": [cargar_pagina],
+                "Ayuda": [cargar_pagina_ayuda]
             }
         )
 
