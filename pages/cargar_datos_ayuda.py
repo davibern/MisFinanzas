3aa722fef1f9a4a299596aad8cd942469d58b1dd
davibern <david.bern.pal@gmail.com>
2026-02-28 12:20:14 +0100

    Añadir info de configurar filtros para incluir movimientos de previsión y jubilación

--- pages/cargar_datos_ayuda.py
+++ pages/cargar_datos_ayuda.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title("📚 Manual de ayuda para importar datos bancarios")
-st.write("A continuación se detalla el proceso para descargar los datos de la cuenta de CaixaBank para que puedas importarlo en la herramienta de __Cargar Datos__.")
+st.write("A continuación se detalla el proceso para descargar los datos de la cuenta de CaixaBank para \
+    que puedas importarlo en la herramienta de __Cargar Datos__.")
 st.markdown("""---""", unsafe_allow_html=True)
 
 st.write("Entramos en https://www.caixabank.es/, con nuestro usuario y contraseña.")
@@ -15,12 +16,18 @@ st.write("Esto nos llevará a la página de movimientos, aquí tenemos que filtr
 st.write("Para ello pulsamos en __Periodo__ seleccionamos el mes, normalmente el mes anterior, y pulsamos en el botón azul __Filtrar__.")
 st.image("./assets/helps/seleccionar_mes.png")
 
+st.info("IMPORTANTE: incluye como filtro los movimientos relacionados con los fondos de previsión y jubilación, \
+    ya que si no los incluyes no se importarán a la herramienta y no podrás llevar un control de ellos.")
+
 st.write("Ahora, el paso importante es __listar todos los movimientos del mes en cuestión para que estén disponibles en la pantalla__.")
-st.write("Para ello con el ratón buscamos un texto con un enlace llamado __ver mas movimientos__ y lo pulsamos tantas veces como necesitemos para mostrar el primer movimiento del mes.")
+st.write("Para ello con el ratón buscamos un texto con un enlace llamado __ver mas movimientos__ y lo pulsamos tantas veces\
+    como necesitemos para mostrar el primer movimiento del mes.")
 st.image("./assets/helps/ver_mas_movimientos.png")
 
-st.write("Ahora que están en la pantalla todos los datos disponibles (ingresos y gastos del mes seleccionado) lo siguiente es volver a la parte de arriba de la página y pulsar en el botón de la esquina superior derecha llamada __exportar__.")
+st.write("Ahora que están en la pantalla todos los datos disponibles (ingresos y gastos del mes seleccionado)\
+    lo siguiente es volver a la parte de arriba de la página y pulsar en el botón de la esquina superior derecha llamada __exportar__.")
 st.image("./assets/helps/exportar.png")
 
-st.write("Por último, seleccionamos la opción __lista de movimientos visualizados en pantalla__ y pulsamos en __aceptar__ y se descargará un fichero .xlm")
+st.write("Por último, seleccionamos la opción __lista de movimientos visualizados en pantalla__ y\
+    pulsamos en __aceptar__ y se descargará un fichero .xlm")
 st.image("./assets/helps/exportar_lista_movimientos.png")
