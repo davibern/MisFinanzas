b73d78f448fac6aa552f76704991c760dfe1ea37
davibern <david.bern.pal@gmail.com>
2026-03-01 13:19:50 +0100

    Añadir opción de menú para incluir acceso a la carga de datos de ahorro

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 def main():
     inicio_pagina = st.Page("./pages/inicio.py", title="Inicio", icon=":material/home:")
     cargar_banco = st.Page("./pages/cargar_banco.py", title="Cargar Banco", icon=":material/file_upload:")
+    cargar_ahorro = st.Page("./pages/cargar_ahorro.py", title="Cargar Ahorro", icon=":material/file_upload:")
     resumen_mensual = st.Page("./pages/resumen_mensual.py", title="Resumen Mensual", icon=":material/stacked_bar_chart:")
     resumen_anual = st.Page("./pages/resumen_anual.py", title="Resumen Anual", icon=":material/bar_chart:")
     ahorro_prevision = st.Page("./pages/ahorro_prevision.py", title="Ahorro y Previsión", icon=":material/savings:")
@@ -13,7 +14,7 @@ def main():
             {
                 "Inicio": [inicio_pagina],
                 "Informes": [resumen_mensual, resumen_anual, ahorro_prevision],
-                "Herramientas": [cargar_banco],
+                "Herramientas": [cargar_banco, cargar_ahorro],
                 "Ayuda": [cargar_banco_ayuda]
             }
         )
