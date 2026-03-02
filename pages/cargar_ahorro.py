37bbf9a250f9043c09fa7d19021cd0d4baa706ec
davibern <david.bern.pal@gmail.com>
2026-03-02 18:35:33 +0100

    Implementar lógica básica para importar y exportar a parquet datos de ahorro según compañía

--- pages/cargar_ahorro.py
+++ pages/cargar_ahorro.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-from src.cargar_datos_ahorros import CargarDatosAhorros as CargarFichero
+from src.cargar_datos_ahorros import CargarFicheroAhorro as CargarFichero
+from src.exportar_datos import ExportarDatos
 
 st.title("🗂️ Cargar Datos de Ahorro")
 
@@ -10,5 +11,8 @@ if archivo is not None:
     # Cargar y procesar el archivo desde el uploader de streamlit
     fichero = CargarFichero(archivo)
     fichero.limpiar_datos()
+
+    exportar = ExportarDatos(fichero, tipo='ahorro', compañia='AXA')
+    exportar.exportar_parquet()
 
     st.dataframe(fichero.df)
