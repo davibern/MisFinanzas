325c8771da8fe126b2fdba3b7fbb82e41c746a0c
davibern <david.bern.pal@gmail.com>
2026-03-03 18:31:42 +0100

    Obtener nombre compañia del fichero

--- pages/cargar_ahorro.py
+++ pages/cargar_ahorro.py
@@ -8,11 +8,14 @@ st.title("🗂️ Cargar Datos de Ahorro")
 archivo = st.file_uploader('Selecciona un archivo CSV', type=['csv'])
 
 if archivo is not None:
+    # Obtener el nombre de la compañia
+    compañia = archivo.name.split('-')[1].strip().split(".")[0].strip()
+
     # Cargar y procesar el archivo desde el uploader de streamlit
     fichero = CargarFichero(archivo)
     fichero.limpiar_datos()
 
-    exportar = ExportarDatos(fichero, tipo='ahorro', compañia='AXA')
+    exportar = ExportarDatos(fichero, tipo='ahorro', compañia=compañia)
     exportar.exportar_parquet()
 
     st.dataframe(fichero.df)
