66965ed35f9298d754008a98435d6807c0a6279b
davibern <david.bern.pal@gmail.com>
2026-03-24 19:39:57 +0100

    Añadir clase para guardar los colores

--- src/config.py
+++ src/config.py
@@ -8,3 +8,17 @@ RAW_DIR = BASE_DIR / "raw"
 # Rutas específicas para constantes
 RUTA_FINANZAS_PARQUET = str(DATA_DIR / "finanzas.parquet")
 RUTA_AHORROS_PARQUET = str(DATA_DIR / "ahorros.parquet")
+
+
+class Color:
+    """Clase para guardar los colores de la aplicación"""
+    AZUL = "#1f77b4"
+    NARANJA = "#ff7f0e"
+    VERDE = "#2ca02c"
+    ROJO = "#d62728"
+    MORADO = "#9467bd"
+    MARRON = "#8c564b"
+    ROSA = "#e377c2"
+    GRIS = "#7f7f7f"
+    OLIVA = "#bcbd22"
+    CIAN = "#17becf"
