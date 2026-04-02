1a1923a7d09164921a3ee3ff62edd0acc4ce2d17
davibern <david.bern.pal@gmail.com>
2026-04-02 18:53:00 +0200

    Eliminar comentarios

--- src/cargar_datos_ahorros.py
+++ src/cargar_datos_ahorros.py
@@ -24,11 +24,11 @@ class CargarFicheroAhorro:
             )
             self.df[col] = pd.to_numeric(self.df[col], errors='coerce')
 
-        # 3. Procesar Fechas (Rellenar los huecos que mencionaste)
+        # 3. Procesar Fechas
         self.df['FECHA'] = pd.to_datetime(self.df['FECHA'], dayfirst=True, errors='coerce')
         self.df['FECHA'] = self.df['FECHA'].ffill()
 
-        # Eliminar filas que sigan sin fecha (como cabeceras corruptas)
+        # Eliminar filas que sigan sin fecha
         self.df = self.df.dropna(subset=['FECHA'])
 
         # Formato de solo fecha
