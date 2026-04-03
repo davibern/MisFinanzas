561a0f0842ccb7d1e819a736a566436e389ef20c
davibern <david.bern.pal@gmail.com>
2026-04-03 21:50:40 +0200

    Crear fichero para la ayuda del flujo de caja

---
+++ pages/cargar_caja_ayuda.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("📚 Manual de ayuda para importar datos de flujo de caja")
+st.write("En breve estará disponible el manual de ayuda para importar datos de flujo de caja.")
