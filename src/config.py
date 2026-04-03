a4ec780c9b95f0e068698b42746c1e16a0b251a1
davibern <david.bern.pal@gmail.com>
2026-04-03 21:50:26 +0200

    Añadir ruta para los datos de caja

--- src/config.py
+++ src/config.py
@@ -8,6 +8,7 @@ RAW_DIR = BASE_DIR / "raw"
 # Rutas específicas para constantes
 RUTA_FINANZAS_PARQUET = str(DATA_DIR / "finanzas.parquet")
 RUTA_AHORROS_PARQUET = str(DATA_DIR / "ahorros.parquet")
+RUTA_CAJA_PARQUET = str(DATA_DIR / "caja.parquet")
 
 
 class Color:
