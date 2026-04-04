d273a45a5faeea57d5a647776c93e56b21f74705
davibern <david.bern.pal@gmail.com>
2026-04-04 12:26:16 +0200

    Eliminar enlace

--- pages/cargar_ahorro_ayuda.py
+++ pages/cargar_ahorro_ayuda.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 st.title("📚 Manual de ayuda para importar datos bancarios")
-st.write("A continuación se detalla el proceso para descargar los datos de la cuenta de CaixaBank para \
-    que puedas importarlo en la herramienta de __Cargar Datos Ahorro__.")
+st.write("A continuación se detalla el proceso para descargar los datos de __Cargar Datos__.")
 st.markdown("""---""", unsafe_allow_html=True)
 
-st.write("Entramos en https://davibernas.quickconnect.to/oo/r/uczNKWLm3qjwKxO6zxOGG8QRKhgHtLHG, con nuestro usuario y contraseña.")
+st.write("Entra en el fichero de datos económicos de tu Synology Nas, con tu usuario y contraseña.")
 st.write("Una vez dentro seleccionamos la hoja de la aseguradora que deseamos descargar")
 st.image("./assets/helps/ahorro_aseguradoras.png")
 
