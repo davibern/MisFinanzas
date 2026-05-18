5da2f7fc7798895866ba0fec67d9e5bb08ec5661
davibern <david.bern.pal@gmail.com>
2026-05-18 19:58:27 +0200

    Unificar página de carga de datos

--- app.py
+++ app.py
@@ -6,22 +6,18 @@ from src.locale import Locale
 def main():
     locale = Locale(st.context.locale)
     inicio_pagina = st.Page("./pages/inicio.py", title=locale.textos["menu"]["inicio"], icon=":material/home:")
-    cargar_banco = st.Page("./pages/cargar_banco.py", title=locale.textos["menu"]["cargar_banco"], icon=":material/file_upload:")
-    cargar_ahorro = st.Page("./pages/cargar_ahorro.py", title=locale.textos["menu"]["cargar_ahorro"], icon=":material/file_upload:")
-    cargar_caja = st.Page("./pages/cargar_caja.py", title=locale.textos["menu"]["cargar_caja"], icon=":material/file_upload:")
+    cargar_datos = st.Page("./pages/cargar_datos.py", title=locale.textos["menu"]["cargar_datos"], icon=":material/file_upload:")
     resumen_mensual = st.Page("./pages/resumen_mensual.py", title=locale.textos["menu"]["resumen_mensual"], icon=":material/stacked_bar_chart:")
     resumen_anual = st.Page("./pages/resumen_anual.py", title=locale.textos["menu"]["resumen_anual"], icon=":material/bar_chart:")
     ahorro_prevision = st.Page("./pages/ahorro_prevision.py", title=locale.textos["menu"]["ahorro_prevision"], icon=":material/savings:")
-    cargar_banco_ayuda = st.Page("./pages/cargar_banco_ayuda.py", title=locale.textos["menu"]["ayuda_datos_banarios"], icon=":material/help:")
-    cargar_ahorro_ayuda = st.Page("./pages/cargar_ahorro_ayuda.py", title=locale.textos["menu"]["ayuda_datos_ahorro"], icon=":material/help:")
-    cargar_caja_ayuda = st.Page("./pages/cargar_caja_ayuda.py", title=locale.textos["menu"]["ayuda_datos_caja"], icon=":material/help:")
+    cargar_datos_ayuda = st.Page("./pages/cargar_datos_ayuda.py", title=locale.textos["menu"]["ayuda_cargar_datos"], icon=":material/help:")
 
     pg = st.navigation(
             {
                 locale.textos["menu"]["inicio"]: [inicio_pagina],
                 locale.textos["menu"]["informes"]: [resumen_mensual, resumen_anual, ahorro_prevision],
-                locale.textos["menu"]["herramientas"]: [cargar_banco, cargar_ahorro, cargar_caja],
-                locale.textos["menu"]["ayuda"]: [cargar_banco_ayuda, cargar_ahorro_ayuda, cargar_caja_ayuda]
+                locale.textos["menu"]["herramientas"]: [cargar_datos],
+                locale.textos["menu"]["ayuda"]: [cargar_datos_ayuda]
             }
         )
 
