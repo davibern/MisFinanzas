6ff28d91ce465028b9929ce79c3ee6135854f466
davibern <david.bern.pal@gmail.com>
2026-08-31 15:32:02 +0200

    Añadir al menú la opción de "Acciones"

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ def main():
     resumen_mensual = st.Page("./pages/resumen_mensual.py", title=locale.textos["menu"]["resumen_mensual"], icon=":material/stacked_bar_chart:")
     resumen_anual = st.Page("./pages/resumen_anual.py", title=locale.textos["menu"]["resumen_anual"], icon=":material/bar_chart:")
     ahorro_prevision = st.Page("./pages/ahorro_prevision.py", title=locale.textos["menu"]["ahorro_prevision"], icon=":material/savings:")
+    acciones = st.Page("./pages/acciones.py", title=locale.textos["menu"]["acciones"], icon=":material/finance_mode:")
     cargar_datos_ayuda = st.Page("./pages/cargar_datos_ayuda.py", title=locale.textos["menu"]["ayuda_cargar_datos"], icon=":material/help:")
 
     pg = st.navigation(
             {
                 locale.textos["menu"]["inicio"]: [inicio_pagina],
                 locale.textos["menu"]["informes"]: [resumen_mensual, resumen_anual, ahorro_prevision],
+                locale.textos["menu"]["acciones"]: [acciones],
                 locale.textos["menu"]["herramientas"]: [cargar_datos],
                 locale.textos["menu"]["ayuda"]: [cargar_datos_ayuda]
             }
