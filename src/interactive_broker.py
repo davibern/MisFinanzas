8a3492c7bf8bf590d5b69048c19bd32ef2e60b22
davibern <david.bern.pal@gmail.com>
2026-09-02 23:35:32 +0200

    Obtener datos de mi cartera y formatear la información antes de enviarla a streamlit

--- src/interactive_broker.py
+++ src/interactive_broker.py
@@ -32,4 +32,41 @@ class InteractiveBroker:
         """
         if not self.esta_conectado():
             return []
-        return self.ib.portfolio()
+
+        cartera_crudo = self.ib.portfolio()
+        cartera_formateada = []
+
+        # valor total de la cartera
+        valor_total_cartera = sum(item.marketValue for item in cartera_crudo)
+
+        for item in cartera_crudo:
+            simbolo: str = item.contract.symbol
+            exchange: str = item.contract.primaryExchange or item.contract.exchange
+            contrato: str = f'{simbolo} ({exchange})'
+            base_coste: float = item.position * item.averageCost
+
+            if base_coste != 0:
+                pct_pyg_no_realizadas: float = (item.unrealizedPNL / base_coste) * 100
+            else:
+                pct_pyg_no_realizadas: float = 0.0
+
+            if valor_total_cartera != 0:
+                pct_liquidacion: float = (item.marketValue / valor_total_cartera) * 100
+            else:
+                pct_liquidacion: float = 0.0
+
+            # Crear diccionario plano para que el st.dataframe lo parsee
+            cartera_formateada.append({
+                "Intrumento": contrato,
+                "Posición": round(item.position, 4),
+                "Último": round(item.marketPrice, 2),
+                "Precio Medio": round(item.averageCost, 2),
+                "Base de Coste": round(base_coste, 2),
+                "Valor Mercado": round(item.marketValue, 2),
+                "% Liquidación Neta": round(pct_liquidacion, 2),
+                "PyG No Realizadas": round(item.unrealizedPNL, 2),
+                "% PyG No Realizadas": round(pct_pyg_no_realizadas, 2),
+                "PyG Realizadas": round(item.realizedPNL, 2)
+            })
+
+        return cartera_formateada
